a688f8c322312ec75aac46e56a8a930e6187e1ff
Marcellofabbri <marcellogfabbri@gmail.com>
2019-12-12 16:57:28 +0000

    added docking station features

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -7,5 +7,16 @@ describe DockingStation do
       bike = subject.release_bike
       expect(bike).to be_working
     end
+
+    it "docks a bike" do
+      bike = Bike.new
+      expect(subject.dock(bike)).to eq bike
+    end
+
+    it "returns a bike" do
+        bike = Bike.new
+        subject.dock(bike)
+        expect(subject.bike).to eq bike
+    end
 end
 

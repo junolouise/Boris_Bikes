584a0226ff83424ce047f6900c334b072665fee4
Student <student@Makerss-MacBook-Pro-5.local>
2019-12-13 11:27:49 +0000

    added feature for docking station to have a fail when no bikes available and added tests

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,11 +4,6 @@ describe DockingStation do
     it { is_expected.to respond_to :release_bike }
     it { is_expected.to respond_to(:dock).with(1).argument }
 
-    it "releases working bikes" do
-      bike = subject.release_bike
-      expect(bike).to be_working
-    end
-
     it "docks a bike" do
       bike = Bike.new 
       expect(subject.dock(bike)).to eq bike
@@ -19,5 +14,19 @@ describe DockingStation do
         subject.dock(bike)
         expect(subject.bike).to eq bike
     end
+
+    describe '#release_bike' do
+       
+        it 'releases a bike' do
+            bike = Bike.new
+            subject.dock(bike)
+            expect(subject.release_bike).to eq bike
+        end
+    
+        it 'raises an error when no bikes are available' do
+            expect { subject.release_bike }.to raise_error 'No bikes available'
+        end
+    end
+
 end
 
